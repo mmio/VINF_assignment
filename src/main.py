a1a7de136fa17fd88d9416f9b3e939cc4c71d3db
Dominik <dominikvasko96@gmail.com>
2020-12-08 17:54:05 +0100

    More data to be collected

--- src/main.py
+++ src/main.py
@@ -42,7 +42,7 @@ def get_query(tsv_stream):
         yield row[1]
 
 def queries_to_vector(nlp, tokenizer, row_stream):
-    # hot = HistogramOfQueries('data/global_stats/')
+    hot = HistogramOfQueries('data/global_stats/')
     query_stream = map(lambda query: query[:-1], row_stream)
 
     uniq_queries = set(query_stream)
@@ -65,7 +65,7 @@ def queries_to_vector(nlp, tokenizer, row_stream):
             continue
 
         ## collect stats for the day
-        # hot.add_doc(doc, 0)
+        hot.add_doc(doc, 0)
 
         dq = tokenizer(without_stopwords)
 
@@ -73,7 +73,7 @@ def queries_to_vector(nlp, tokenizer, row_stream):
 
         yield dq.vector, dqq.vector, dq.text, dqq.text
 
-    # hot.save()
+    hot.save()
 
 def reduce_dimensions(data, n_components):
     pca_of_n = PCA(n_components)
@@ -167,11 +167,11 @@ def main():
     #     # DictionaryOfTokens('data/users/global/stats/dict.pickle'),
     # ]
 
-    # divide_queries_based_on_time(get_text_from_gzip(archives))
+    divide_queries_based_on_time(get_text_from_gzip(archives))
     
     path = f'data/dates/'
     days = os.listdir(path)
-    day_batches = list(equality_divide_array(days, 2))
+    day_batches = list(equality_divide_array(days, 1))
     
     # with concurrent.futures.ThreadPoolExecutor() as executor:
     #     futures = []
@@ -215,6 +215,8 @@ def process(path, folders):
         
         ls = []
         ls_norm = []
+        data_for_sne = []
+        data_norm_for_sne = []
         for coll in iter_by_batch(iter(vectors), batch_size):
             unzipped = list(zip(*coll))
 
@@ -225,6 +227,9 @@ def process(path, folders):
 
             ls.extend(zip(cl_model.predict(data), queries))
             ls_norm.extend(zip(cl_model_norm.predict(data_norm), queries_norm))
+
+            data_for_sne.extend(data)
+            data_norm_for_sne.extend(data_norm)
         
         print('writing')
         with open(f'readme{folder}.txt', 'w') as f:
@@ -235,37 +240,55 @@ def process(path, folders):
             for item in ls_norm:
                 f.write(f'{str(item)}\n')
                 
-        # labels_norm = vector_to_scatterplot(data_norm, queries_norm, folder, cl_model, sufix='_norm')
-        # with open(f'{path}{folder}/cluster_similarity', 'w') as f:
-            # f.write(str(adjusted_rand_score(labels, labels_norm)))
+        # compare clusters of normalized and non-normalized queries
+        results = list(zip(*ls))
+        labels = results[0]
+        query_subset = results[1]
 
+        results_norm = list(zip(*ls_norm))
+        labels_norm = results_norm[0]
+        query_subset_norm = results_norm[1]
 
-        # os.mkdir(f'data/dates/{savefolder}/clusters{sufix}')
-        # for cluster_id, query in zip(clustered_data.labels_, query_subset):
-        #     with open(f'data/dates/{savefolder}/clusters{sufix}/{cluster_id}', 'a') as f:
-        #         f.write(f'{query}\n')
+        with open(f'{path}{folder}/cluster_similarity', 'w') as f:
+            f.write(str(adjusted_rand_score(labels, labels_norm)))
 
-        # with open(f'data/dates/{savefolder}/e{e}-s{s}-result_queries{sufix}.txt', 'w') as f:
-        #     for pair in zip(clustered_data.labels_, query_subset):
-        #         f.write(f'{str(pair)}\n')
+        # save cluster queries
+        os.mkdir(f'{path}{folder}/clusters')
+        for cluster_id, query in ls:
+            with open(f'{path}{folder}/clusters/{cluster_id}', 'a') as f:
+                f.write(f'{query}\n')
 
-        # print("tsne start")
-        # tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=1000, n_jobs=1)
-        # tsne_results = tsne.fit_transform(reduced_data)
-        # print("tsne finished")
+        os.mkdir(f'{path}{folder}/clusters_norm')
+        for cluster_id, query in ls_norm:
+            with open(f'{path}{folder}/clusters_norm/{cluster_id}', 'a') as f:
+                f.write(f'{query}\n')
 
-        # with open(f'data/dates/{savefolder}/cluster_labels{sufix}.txt', 'w') as f:
-        #     f.write(str(clustered_data.labels_))
-        # save_scatterplot(f'data/dates/{savefolder}/e{e}-s{s}-{savefolder}{sufix}.pdf', tsne_results[:,0], tsne_results[:,1], clustered_data.labels_)
+        print("tsne start")
+        tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=1000, n_jobs=1)
+        tsne_results = tsne.fit_transform(data_for_sne)
+        print("tsne finished")
+        save_scatterplot(f'data/dates/{folder}/{folder}.pdf', tsne_results[:,0], tsne_results[:,1], labels)
 
-        # save cluster data to folder, for further comparison
-        # os.mkdir(f'data/dates/{savefolder}/cluster_dump{sufix}')
+        print("tsne start")
+        tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=1000, n_jobs=1)
+        tsne_results = tsne.fit_transform(data_norm_for_sne)
+        print("tsne finished")
+        save_scatterplot(f'data/dates/{folder}/{folder}_norm.pdf', tsne_results[:,0], tsne_results[:,1], labels_norm)
 
-        # for label in set(labels):
-        #     with open(f'data/dates/{savefolder}/cluster_dump{sufix}/{label}', 'wb') as fh:
-        #         for i in range(len(labels)):
-        #             if labels[i] == label:
-        #                 pickle.dump(data_subset[i], fh)
+        # save cluster of vectors data to folder, for further comparison
+        os.mkdir(f'{path}{folder}/cluster_dump')
+        for label in set(labels):
+            with open(f'{path}{folder}/cluster_dump/{label}', 'wb') as fh:
+                for i in range(len(labels)):
+                    if labels[i] == label:
+                        pickle.dump(data_for_sne[i], fh)
+
+        os.mkdir(f'{path}{folder}/cluster_dump_norm')
+        for label in set(labels_norm):
+            with open(f'{path}{folder}/cluster_dump_norm/{label}', 'wb') as fh:
+                for i in range(len(labels_norm)):
+                    if labels_norm[i] == label:
+                        pickle.dump(data_norm_for_sne[i], fh)
 
 
 if __name__ == '__main__':
