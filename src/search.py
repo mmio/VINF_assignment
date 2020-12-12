0d1d861e06a0a2fcf5848386c5097c0a8e2b0f72
Dominik <dominikvasko96@gmail.com>
2020-12-13 00:47:19 +0100

    Add search

--- src/search.py
+++ src/search.py
@@ -1,5 +1,8 @@
+import os
 import sys
 import pickle
+import itertools
+from sklearn.feature_extraction.text import TfidfVectorizer
 # import lucene
  
 # from java.io import File
@@ -26,83 +29,107 @@ def read_from_pickle(path):
             pass
 
 if __name__ == "__main__":
-    nlp = get_pipe()
-    tokenizer = get_tokenizer(nlp)
-    cluster_vecs = None
-    with open(f'data/dates/3_1/cluster_w2v/0') as f:
-        cluster_vecs = [doc.vector for doc in nlp.pipe(f, disable=['tagger', 'ner', 'language'])]
-    search_term_vec = tokenizer('homes').vector.reshape(1, -1)
+    # nlp = get_pipe()
+    # tokenizer = get_tokenizer(nlp)
+    # cluster_vecs = None
+    # with open(f'data/dates/3_1/cluster_w2v/0') as f:
+    #     cluster_vecs = [doc.vector for doc in nlp.pipe(f, disable=['tagger', 'ner', 'language'])]
+    # search_term_vec = tokenizer('homes').vector.reshape(1, -1)
 
-    print(cosine_similarity(search_term_vec, cluster_vecs))
-    print(np.mean(cosine_similarity(search_term_vec, cluster_vecs)))
+    # print(cosine_similarity(search_term_vec, cluster_vecs))
+    # print(np.mean(cosine_similarity(search_term_vec, cluster_vecs)))
 
-    # lucene.initVM()
-    # analyzer = StandardAnalyzer()
+    lucene.initVM()
+    analyzer = StandardAnalyzer()
 
-    # subfolder_index_pairs = [
-    #     ('cluster_w2v', 'index_w2v'),
-    #     ('cluster_w2v_n', 'index_w2v_n'),
-    #     ('cluster_tfidf', 'index_tfidf'),
-    #     ('cluster_tfidf_n', 'index_tfidf_n')
-    # ]
+    subfolder_index_pairs = [
+        ('cluster_w2v', 'index_w2v'),
+        ('cluster_w2v_n', 'index_w2v_n'),
+        ('cluster_tfidf', 'index_tfidf'),
+        ('cluster_tfidf_n', 'index_tfidf_n')
+    ]
 
-    # search_term = sys.argv[1]
+    search_term = sys.argv[1]
 
-    # for cluster_type, index in subfolder_index_pairs:
-    #     print(f'searching in {index}')
-    #     path = Paths.get(index)
+    for cluster_type, index in subfolder_index_pairs:
+        print(f'searching in {index}')
+        path = Paths.get(index)
         
-    #     reader = DirectoryReader.open(SimpleFSDirectory(path))
-    #     searcher = IndexSearcher(reader)
+        reader = DirectoryReader.open(SimpleFSDirectory(path))
+        searcher = IndexSearcher(reader)
  
-    #     query = QueryParser("content", analyzer).parse(search_term)
-    #     MAX = 1000000
-    #     hits = searcher.search(query, MAX)
+        query = QueryParser("content", analyzer).parse(search_term)
+        MAX = 1000000
+        hits = searcher.search(query, MAX)
  
-    #     month_counter = [
-    #         [0] * 32,
-    #         [0] * 32,
-    #         [0] * 32
-    #     ]
+        month_counter = [
+            [0] * 32,
+            [0] * 32,
+            [0] * 32
+        ]
 
-    #     for hit in hits.scoreDocs:
-    #         doc = searcher.doc(hit.doc)
+        for hit in hits.scoreDocs:
+            doc = searcher.doc(hit.doc)
 
-    #         month = int(doc.get('day').split('_')[0])
-    #         day = int(doc.get('day').split('_')[1])
+            month = int(doc.get('day').split('_')[0])
+            day = int(doc.get('day').split('_')[1])
 
-    #         month_counter[month-3][day] += 1
+            month_counter[month-3][day] += 1
 
-    #         cluster = doc.get('cluster')
-    #         print(month, day, cluster)
+            cluster = doc.get('cluster')
+            print(month, day, cluster)
 
-    #     flat_months = [item for sublist in month_counter for item in sublist]
-    #     fig, ax = plt.subplots(figsize=(30,8))
-    #     b1 = ax.bar(range(len(flat_months)), flat_months)
+        flat_months = [item for sublist in month_counter for item in sublist]
+        fig, ax = plt.subplots(figsize=(30,8))
+        b1 = ax.bar(range(len(flat_months)), flat_months)
 
-    #     labels = []
-    #     for month in [3,4,5]:
-    #         for day in range(32):
-    #             labels.append(f'{month}_{day}')
+        labels = []
+        for month in [3,4,5]:
+            for day in range(32):
+                labels.append(f'{month}_{day}')
 
-    #     plt.xticks(range(len(flat_months)), (labels), rotation=90, fontsize='xx-small')
-    #     plt.savefig('search_result.pdf', bbox_inches='tight')
-    #     plt.close()
+        plt.xticks(range(len(flat_months)), (labels), rotation=90, fontsize='xx-small')
+        plt.savefig('search_result.pdf', bbox_inches='tight')
+        plt.close()
 
 
-    #     nlp = get_pipe()
-    #     tokenizer = get_tokenizer(nlp)
-    #     # calculate similarity to cluster
-    #     for hit in hits.scoreDocs:
-    #         # print(hit.score, hit.doc, hit.toString())
-    #         doc = searcher.doc(hit.doc)
+        nlp = get_pipe()
+        tokenizer = get_tokenizer(nlp)
 
-    #         day = doc.get('day')
-    #         cluster = doc.get('cluster')
+        path = 'data/dates/'
 
-    #         cluster_vecs = list(read_from_pickle(f'data/dates/{day}/cluster_{cluster_type}_dump/{cluster}'))
-    #         search_term_vec = tokenizer(search_term).vector
+        files = [
+            open(f'{path}{folder}/queries')
+            for folder in os.listdir(path)
+        ]
+        def learn_tfidf_2(stream):
+            vectorizer = TfidfVectorizer(max_features=300)
+            return vectorizer.fit(stream)
+        tfidf = learn_tfidf_2(list(itertools.chain(*files)))
 
-    #         print(cosine_similarity(search_term_vec, cluster_vecs))
+        # calculate similarity to cluster
+        for hit in hits.scoreDocs:
+            # print(hit.score, hit.doc, hit.toString())
+            doc = searcher.doc(hit.doc)
 
-    #         print(day, cluster)
+            day = doc.get('day')
+            cluster = doc.get('cluster')
+
+            cluster_vecs = None
+            search_term_vec = None
+            with open(f'data/dates/{day}/cluster_{cluster_type}/{cluster}') as f:
+                file_content = f.read()
+                if cluster_type in ['cluster_w2v', 'cluster_w2v_n']:
+                    print('in w2v')
+                    cluster_vecs = [doc.vector for doc in nlp.pipe(file_content, disable=['tagger', 'ner', 'language'])]
+                    search_term_vec = tokenizer(search_term).vector.reshape(1, -1)
+                else:
+                    cluster_vecs = tfidf.transform(file_content).toarray()
+                    search_term_vec = tfidf.transform(search_term).toarray()
+
+                similarities = cosine_similarity(search_term_vec, cluster_vecs)
+                
+                max10 = np.argpartition(similarities, -10)[-4:]
+                print(similarities[max10])
+
+
